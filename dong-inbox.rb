adc9cc4fb8b558e39dac4dfc9261e2617123c511
ruibin3 <ruibin3@staff.weibo.com>
2026-03-24 18:10:01 +0800

    fix: use precompiled binaries

--- dong-inbox.rb
+++ dong-inbox.rb
@@ -4,13 +4,20 @@
 class DongInbox < Formula
   desc "inBox 笔记命令行工具"
   homepage "https://github.com/dong-labs/inbox-cli"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/dong-labs/inbox-cli/releases/download/v#{version}/dong-inbox-arm64.tar.gz"
+      sha256 :no_check
+    else
+      url "https://github.com/dong-labs/inbox-cli/releases/download/v#{version}/dong-inbox-x64.tar.gz"
+      sha256 :no_check
+    end
+  end
+
   version "1.0.0"
-  url "https://github.com/dong-labs/inbox-cli/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "537a2aa99dd72d9f7aec1ead8ace8f80138127d76019e546bcf169f67956eefe"
 
   def install
-    system "flutter", "pub", "get"
-    system "dart", "compile", "exe", "bin/inbox.dart", "-o", "dong-inbox"
     bin.install "dong-inbox" => "inbox"
   end
 
