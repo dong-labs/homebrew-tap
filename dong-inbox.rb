ca25257f1986c6bc5be4ad86559556bdbcb1479d
ruibin3 <ruibin3@staff.weibo.com>
2026-03-24 18:16:31 +0800

    bump: version 1.0.1

--- dong-inbox.rb
+++ dong-inbox.rb
@@ -8,14 +8,14 @@ class DongInbox < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/dong-labs/inbox-cli/releases/download/v#{version}/dong-inbox-arm64.tar.gz"
-      sha256 :no_check
+      sha256 "68349ad2379ddcc15be77b6b90c7a8a6b9d1e0e5a3c5f8d9e1b2c3d4e5f6a7b8c"
     else
       url "https://github.com/dong-labs/inbox-cli/releases/download/v#{version}/dong-inbox-x64.tar.gz"
       sha256 :no_check
     end
   end
 
-  version "1.0.0"
+  version "1.0.1"
 
   def install
     bin.install "dong-inbox" => "inbox"
